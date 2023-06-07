c30585fdacc82f83dddc496f54f426b0fb35dbc2
avilovmaksim5 <avilovmaksim5@mail.ru>
2023-06-07 20:48:35 +0400

    update 07/06/23

---
+++ pages/products.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html lang="ru">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport"
+              content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link rel="stylesheet" href="../assets/styles/products.css">
+        <title>Все товары</title>
+
+    </head>
+    <body>
+        <?php require '../moduls/header.php'?>
+
+        <div class="container">
+            <div class="item">
+                <img src="" alt="">
+                <h3>Title</h3>
+                <p>Description Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet consequatur cupiditate distinctio, expedita ipsum tempore!</p>
+                <div class="more">
+                    <span>Price</span>
+                    <a href="">Узнать больше</a>
+                </div>
+
+
+            </div>
+
+        </div>
+    </body>
+</html><?php
+
